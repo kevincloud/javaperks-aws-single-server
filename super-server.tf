08a4928843c933472d76e50db5dba01d4757c474
Kevin Cochran <kcochran@hashicorp.com>
2019-11-04 14:44:17 -0500

    open ldap port

--- super-server.tf
+++ super-server.tf
@@ -108,6 +108,13 @@ resource "aws_security_group" "hashi-server-sg" {
     }
 
     ingress {
+        from_port = 389
+        to_port = 389
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
         from_port = 4646
         to_port = 4648
         protocol = "tcp"
