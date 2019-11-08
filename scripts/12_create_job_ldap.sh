2c4211b9aa42e50d3402e5fff213a58bc248e3bb
Kevin Cochran <kcochran@hashicorp.com>
2019-11-07 19:51:00 -0500

    update

--- scripts/12_create_job_ldap.sh
+++ scripts/12_create_job_ldap.sh
@@ -91,3 +91,18 @@ echo "Done."
 # we need to sleep for just a few seconds
 
 sleep 5
+
+STATUS=""
+SVCCNT=0
+while [ "$STATUS" != "top" ]; do
+    SVCCNT=$(($SVCCNT + 1))
+    if [ $SVCCNT -gt 20 ]; then
+        echo "...timeout checking ldap"
+        break
+    fi
+    sleep 2
+    STATUS=$(curl -s ldap://$CLIENT_IP:389 | sed -n -e '0,/^\tobjectClass/p' | awk -F ": " '{print $2}' | sed '/^$/d')
+    if [ "$STATUS" != "top" ]; then
+        echo "...checking openldap again"
+    fi
+done
