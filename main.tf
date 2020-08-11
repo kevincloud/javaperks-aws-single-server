edf7a74102b9b193f7ce5c382cb9be0c010ceef8
Kevin Cochran <kcochran@hashicorp.com>
2020-08-11 09:28:01 -0400

    add session token

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    token = var.aws_session_token
+    region = var.aws_region
 }
 
 resource "aws_vpc" "primary-vpc" {
